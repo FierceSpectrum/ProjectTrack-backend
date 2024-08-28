2bade38a57b6c72f25fc4170edd4e016d122bdfb
FierceSpectrum <benjaminsandi@hotmail.com>
2024-08-28 16:14:39 -0600

    Creating the API
    Creating the API and configuring the database connection

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const { sequelize } = require("./config/database");
+const cors = require("cors");
+
+// Impotacion de Routes
+
+
+// Impotacion de Middlewares
+
+// App
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+// app.use("Ruta", fuction);
+
+
+// Sincroniza los modelos con la base de datos
+sequelize
+  .sync({ alter: true }) // Usa `alter: true` para ajustar las tablas existentes
+  .then(() => {
+    console.log("Database synchronized");
+    // Puedes iniciar el servidor aquí después de la sincronización
+    const PORT = process.env.PORT || 3002;
+    app.listen(PORT, "0.0.0.0", () =>
+      console.log(`Server running on port ${PORT}!`)
+    );
+    // Impot Scripts
+  })
+  .catch((error) => {
+    console.error("Error syncing database:", error);
+  });
