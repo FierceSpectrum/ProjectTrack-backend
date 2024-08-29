c86f53e7ecc0033fa48745ffc1085a6baa080035
RoneyVaLo <108905119+RoneyVaLo@users.noreply.github.com>
2024-08-29 16:34:33 -0600

    update functionalities for assignment controller

--- src/controllers/assignmentController.js
+++ src/controllers/assignmentController.js
@@ -1,10 +1,19 @@
 import Assignment from "../models/assignmentModel.js";
 
-// Funciones básicas: create, update, post, delete
+// TODO: Probar con la base de datos que todo funcione exitosamente
 const postAssignment = async (req, res) => {
   try {
-    const nuevaAssignment = await Assignment.create(req.body);
-    res.status(201).json(nuevaAssignment);
+    const { permission_id, name, description } = req.body;
+
+    if (permission_id && name && description) {
+      const newAssignment = await Assignment.create({
+        permission_id,
+        name,
+        description
+      });
+      res.status(201).json({ message: "Assignment create successfully!", newAssignment });
+    }
+
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
@@ -21,7 +30,8 @@ const getAssignment = async (req, res) => {
 
 const getAssignmentByID = async (req, res) => {
   try {
-    const assignment = await Assignment.findByPk(req.params.id);
+    const { id } = req?.params
+    const assignment = await Assignment.findByPk(id);
     if (assignment) {
       res.status(200).json(assignment);
     } else {
@@ -34,10 +44,15 @@ const getAssignmentByID = async (req, res) => {
 
 const patchAssignment = async (req, res) => {
   try {
-    const assignment = await Assignment.findByPk(req.params.id);
+    const { id } = req?.params
+    const assignment = await Assignment.findByPk(id);
     if (assignment) {
-      const updatedAssignment = await assignment.update(req.body);
-      res.status(200).json(updatedAssignment);
+      const { name, description } = req.body;
+      const updatedAssignment = await assignment.update({
+        name: name ? name : assignment.name,
+        description: description ? description : assignment.description,
+      });
+      res.status(200).json({ message: "Assignment updated successfully!", updatedAssignment });
     } else {
       res.status(404).json({ message: "Assignment not found" });
     }
@@ -48,10 +63,11 @@ const patchAssignment = async (req, res) => {
 
 const deleteAssignment = async (req, res) => {
   try {
-    const assignment = await Assignment.findByPk(req.params.id);
+    const { id } = req?.params
+    const assignment = await Assignment.findByPk(id);
     if (assignment) {
       await assignment.destroy();
-      res.status(204).json({ message: "Assignment deleted" });
+      res.status(204).json({ message: "Assignment deleted successfully" });
     } else {
       res.status(404).json({ message: "Assignment not found" });
     }
