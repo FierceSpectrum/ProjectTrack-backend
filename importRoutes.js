7ef72ae7ff9ea28a70325effa23f626c4f8aadd8
Benjamín Sandí <127576883+FierceSpectrum@users.noreply.github.com>
2024-09-04 01:41:25 -0600

    index routes
    move routes to index routes and update the script

--- importRoutes.js
+++ importRoutes.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 // Directorio a leer (puedes cambiarlo según tus necesidades)
 const routesDir = path.join(__dirname, "src/routes");
-const indexFile = path.join(__dirname, "index.js");
+const indexFile = path.join(__dirname, "src/routes/index.js");
 
 // Función para leer los archivos de una carpeta
 function readFilesFromDirectory(directory, extension = ".js") {
@@ -24,16 +24,16 @@ function readFilesFromDirectory(directory, extension = ".js") {
 }
 
 // Función para generar las declaraciones de importación y uso
-function generateStatements(files, baseDir) {
+function generateStatements(files) {
   const imports = files.map((file) => {
     const routeName = path.basename(file, ".js");
-    return `import ${routeName} from "./${baseDir}/${file}";`;
+    return `import ${routeName} from "./${file}";`;
   });
 
   const uses = files.map((file) => {
     const importName = path.basename(file, ".js");
     const routeName = path.basename(file, "Routes.js") + "s";
-    return `app.use("/api/${routeName}", ${importName});`;
+    return `router.use("/${routeName}", ${importName});`;
   });
 
   return { imports, uses };
@@ -81,7 +81,7 @@ function updateSection(tag, newStatements, fileContent) {
 async function updateIndexFile() {
   try {
     const routeFiles = await readFilesFromDirectory(routesDir);
-    const { imports, uses } = generateStatements(routeFiles, "src/routes");
+    const { imports, uses } = generateStatements(routeFiles);
 
     fs.readFile(indexFile, "utf8", (err, data) => {
       if (err) {
@@ -112,109 +112,3 @@ async function updateIndexFile() {
 
 // Ejecutar la función principal
 updateIndexFile();
-
-/* // Leer los nombres de los archivos en la carpeta especificada
-fs.readdir(routesDir, (err, files) => {
-  if (err) {
-    console.error("Error leyendo la carpeta:", err);
-    return;
-  }
-
-  // Filtrar solo archivos JavaScript
-  const routeFiles = files.filter((file) => file.endsWith(".js"));
-
-  // Crear contenido para importar y usar las rutas (como arrays)
-  const importStatements = routeFiles.map((file) => {
-    const routeName = path.basename(file, ".js");
-    return `import ${routeName} from './src/routes/${file}';`;
-  });
-
-  const useStatements = routeFiles.map((file) => {
-    const importName = path.basename(file, ".js");
-    const routeName = path.basename(file, "Routes.js") + "s";
-    return `app.use('/api/${routeName}', ${importName});`;
-  });
-
-  // Leer el contenido actual del archivo index.js
-  fs.readFile(indexFile, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error escribiendo el archivo de salida:", err);
-      return;
-    }
-
-    // Función para actualizar la sección de manera segura, evitando duplicados
-    const updateSection = (tag, newStatements, fileContent) => {
-      const lines = fileContent.split("\n");
-      // console.log(lines);
-      const tagIndex = lines.indexOf(tag);
-
-      if (tagIndex === -1) {
-        console.error(
-          `No se encontró la etiqueta "${tag}" en el archivo index.js.`
-        );
-        return fileContent;
-      }
-
-      // Encontrar la posición después del primer salto de línea en blanco
-      let endTagIndex = tagIndex;
-
-      const afterTagContent = lines.slice(tagIndex);
-      const firstBlankLineIndex = afterTagContent.indexOf("");
-      if (firstBlankLineIndex !== -1) {
-        endTagIndex += firstBlankLineIndex;
-      } else {
-        endTagIndex = lines.length;
-      }
-
-      // Obtener el contenido actual de la sección para revisar duplicados
-      const beforeTag = lines.slice(0, tagIndex + 1).join("\n");
-      const currentSection = lines.slice(tagIndex + 1, endTagIndex);
-      const aftersecction = lines.slice(endTagIndex).join("\n");
-
-      // Crear un set de las líneas actuales en la sección
-      const currentLines = currentSection.map((line) => line.trim());
-      // Filtrar líneas que no estén en el newStatements y las pone al final.
-      const uniqueStatements = currentLines.filter(
-        (line) => !newStatements.includes(line)
-      );
-
-      // console.log(uniqueStatements);
-      // process.exit(1);
-
-      if (uniqueStatements.length === 0) {
-        console.log(`No hay nuevas líneas para añadir después de "${tag}".`);
-        return fileContent; // No hacer nada si no hay nuevas líneas
-      }
-
-      newStatements.push(...uniqueStatements);
-
-      // Añadir las líneas únicas a la sección existente
-      const updatedSection = newStatements.join("\n");
-
-      return `${beforeTag}\n${updatedSection}\n${aftersecction}`;
-    };
-
-    // Actualizar secciones de importación y uso de rutas
-    let updatedContent = data;
-    updatedContent = updateSection(
-      "// Importación de Routes",
-      importStatements,
-      updatedContent
-    );
-    updatedContent = updateSection(
-      "// Use Routes",
-      useStatements,
-      updatedContent
-    );
-
-    // Escribir el nuevo contenido en el archivo index.js
-    fs.writeFile(indexFile, updatedContent, "utf8", (err) => {
-      if (err) {
-        console.error("Error escribiendo el archivo index.js:", err);
-        return;
-      }
-      console.log("index.js actualizado con rutas.");
-    });
-  });
-});
- */
--- index.js
+++ index.js
@@ -5,17 +5,8 @@ import { sequelize } from "./src/config/database.js";
 
 dotenv.config();
 
-// Importación de Routes
-import assignmentRoutes from "./src/routes/assignmentRoutes.js";
-import memberRoutes from "./src/routes/memberRoutes.js";
-import organizationRoutes from "./src/routes/organizationRoutes.js";
-import participantRoutes from "./src/routes/participantRoutes.js";
-import permissionRoutes from "./src/routes/permissionRoutes.js";
-import projectRoutes from "./src/routes/projectRoutes.js";
-import roleRoutes from "./src/routes/roleRoutes.js";
-import stateRoutes from "./src/routes/stateRoutes.js";
-import taskRoutes from "./src/routes/taskRoutes.js";
-import userRoutes from "./src/routes/userRoutes.js";
+// Importación de IndexRoutes
+import indexRoutes from "./src/routes/index.js";
 
 // Impotacion de Middlewares
 
@@ -29,17 +20,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Use Routes
-app.use("/api/assignments", assignmentRoutes);
-app.use("/api/members", memberRoutes);
-app.use("/api/organizations", organizationRoutes);
-app.use("/api/participants", participantRoutes);
-app.use("/api/permissions", permissionRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/roles", roleRoutes);
-app.use("/api/states", stateRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/users", userRoutes);
+// Use IndexRoutes
+app.use("/api", indexRoutes);
 
 // Sincroniza los modelos con la base de datos
 sequelize
